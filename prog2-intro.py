e59b02c5741d48e04a472de8547e898b3195e6b2
Kevin Edin <kevin.edin@elev.ga.ntig.se>
2025-08-22 10:29:01 +0200

    pluh

---
+++ prog2-intro.py
@@ -0,0 +1,12 @@
+grade = "B"
+best_font = "Comic Sans"
+
+while True:
+    answer = input("Vilken är den bästa fonten?: ")
+    if answer == "Comic Sans":
+        print("WOW! Vad rätt du har! Du får A!")
+        grade = "A"
+    else:
+        print("WOW! Vad fel du har! Du får F!")
+        grade = "F"
+    print(f"Du fick {grade} som slut betyg.")
